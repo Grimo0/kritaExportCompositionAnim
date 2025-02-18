aadfaabdb3efb38aaba55f31cd3c825aafedb90d
Grégoire André <gregoir.andre@gmail.com>
2025-02-18 13:13:16 -0300

    Remove files before creating new links with the bat

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,8 @@
+rmdir /S "%USERPROFILE%\AppData\Roaming\krita\pykrita\exportLayerAnim"
 mklink /D "%USERPROFILE%\AppData\Roaming\krita\pykrita\exportLayerAnim" %~dp0\exportLayerAnim
+del "%USERPROFILE%\AppData\Roaming\krita\pykrita\exportLayerAnim.desktop"
 mklink "%USERPROFILE%\AppData\Roaming\krita\pykrita\exportLayerAnim.desktop" %~dp0\exportLayerAnim.desktop
 mkdir "%USERPROFILE%\AppData\Roaming\krita\actions"
+del "%USERPROFILE%\AppData\Roaming\krita\actions\exportLayerAnim.action"
 mklink "%USERPROFILE%\AppData\Roaming\krita\actions\exportLayerAnim.action" %~dp0\exportLayerAnim.action
 pause
